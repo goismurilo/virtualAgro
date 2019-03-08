24c4fee4ae0e02d0b74b09b3ccd0ed7b46af0283
MuriloGois <murilogois59@gmail.com>
2019-03-07 21:44:02 -0300

    t
    mudei algumas coisas

---
+++ editarAnuncio.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+session_start();
+
+include('tools.php');
+
+if(isset($_SESSION['id'])){ //SE ESTIVER LOGADO
+
+
+if(meuAnuncio(getIdAnuncianteIdAnuncio($_GET['idAnuncio']), $_SESSION['id'])){
+	ECHO "ESSE ANÚNCIO É SEU";
+}else{
+	ECHO "ESSE ANÚNCIO NÃO TE PERTENCE";
+        ECHO "TESTE GIT";
+}
+
+
+} else{
+
+	ECHO "OPERAÇÃO INVÁLIDA";
+}
+?>
